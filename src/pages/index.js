a7e0c577ee456bb56dc82eb105d4352c87fd5f1e
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-01-18 21:43:13 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ import styles from "./styles.module.css";
 //Components
 import ExploreContent from "../components/ExploreContent/ExploreContent";
 //import DiscoverYourPath from "../components/DiscoverYourPath/DiscoverYourPath";
+import { DocSearch } from '@docsearch/react'
 
 function Home() {
   const context = useDocusaurusContext();
